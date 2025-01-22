b2192cb5943a551b5862b14ed0905ea6040ec637
begalievn <begaliev.info@gmail.com>
2025-01-22 12:07:24 +0600

    refactor upload function

--- packages/functions/src/interfaces/storage-service.interface.ts
+++ packages/functions/src/interfaces/storage-service.interface.ts
@@ -1,5 +1,5 @@
 export interface IStorageService {
-  uploadFile(fileKey: string, file: Buffer): Promise<void>;
   deleteFile(fileKey: string): Promise<void>;
   getSignedUrl(fileKey: string): Promise<string>;
+  getPresignedUploadUrl(fileKey: string): Promise<string>;
 }
--- packages/functions/src/services/storage.service.ts
+++ packages/functions/src/services/storage.service.ts
@@ -1,4 +1,4 @@
-import { S3Client, DeleteObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client, DeleteObjectCommand, GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { IStorageService } from "../interfaces/storage-service.interface";
 import { Resource } from "sst";
@@ -6,6 +6,8 @@ import { Resource } from "sst";
 const s3Client = new S3Client({});
 
 export class StorageService implements IStorageService {
+  private bucketName = Resource.Uploads.name;
+
   async deleteFile(fileKey: string): Promise<void> {
     const params = { Bucket: Resource.Uploads.name, Key: fileKey };
     await s3Client.send(new DeleteObjectCommand(params));
@@ -15,7 +17,14 @@ export class StorageService implements IStorageService {
     return getSignedUrl(s3Client, new GetObjectCommand({ Bucket: Resource.Uploads.name, Key: fileKey }), { expiresIn: 3000 });
   }
 
-  async uploadFile(fileKey: string, file: Buffer): Promise<void> {
-    return;
+  async getPresignedUploadUrl(fileKey: string): Promise<string> {
+    return getSignedUrl(
+      s3Client,
+      new PutObjectCommand({
+        Bucket: this.bucketName,
+        Key: fileKey,
+      }),
+      { expiresIn: 300 }
+    );
   }
 }
